17ede03984bafdc1e4987561448c82397b4c7ecf
UrielAndrade <andradeleiru@gmail.com>
2026-05-19 13:13:45 -0400

    add Swagger UI route to main application

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 	"estoque-go/handlers"
 
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+
+	_ "estoque-go/docs"
 )
 
 func main() {
 	r := mux.NewRouter()
+	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
 	r.HandleFunc("/products", handlers.GetProducts).Methods("GET")
 	r.HandleFunc("/products", handlers.CreateProduct).Methods("POST")
