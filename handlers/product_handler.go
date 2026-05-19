e976874935201775e5c3bbdcfd412a19770aa0b5
UrielAndrade <andradeleiru@gmail.com>
2026-05-19 11:55:22 -0400

    add product handler with CRUD operations

---
+++ handlers/product_handler.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"estoque-go/models"
+	"estoque-go/storage"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
+	json.NewDecoder(r.Body).Decode(&product)
+
+	product.ID = storage.NextID
+	storage.NextID++
+
+	storage.Products = append(storage.Products, product)
+
+	json.NewEncoder(w).Encode(product)
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(storage.Products)
+}
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	for i, p := range storage.Products {
+		if p.ID == id {
+			json.NewDecoder(r.Body).Decode(&storage.Products[i])
+			storage.Products[i].ID = id
+			json.NewEncoder(w).Encode(storage.Products[i])
+			return
+		}
+	}
+
+	http.Error(w, "Produto não encontrado", http.StatusNotFound)
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	for i, p := range storage.Products {
+		if p.ID == id {
+			storage.Products = append(storage.Products[:i], storage.Products[i+1:]...)
+			w.Write([]byte("Deletado com sucesso"))
+			return
+		}
+	}
+
+	http.Error(w, "Produto não encontrado", http.StatusNotFound)
+}
