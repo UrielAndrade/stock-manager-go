e1c118f45d3bf6c3461fada991c12e8ef665a42c
UrielAndrade <andradeleiru@gmail.com>
2026-05-20 11:41:43 -0400

    feat: update User model to include auto-increment ID and additional fields for password and email

--- internal/models/User.go
+++ internal/models/User.go
@@ -1,8 +1,10 @@
 package models
 
 type User struct {
-	Id       int    `json:"id" gorm:"primaryKey"`
+	Id       int    `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name     string `json:"name"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 	Birthday string `json:"birthday"`
 	Address  string `json:"address"`
 	Cpf      string `json:"cpf"`
