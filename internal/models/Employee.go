53003a1cf076b97973dceb5f601f3922af2f265a
UrielAndrade <andradeleiru@gmail.com>
2026-05-20 11:41:38 -0400

    feat: update Employee model to include auto-increment ID and foreign key for user

--- internal/models/Employee.go
+++ internal/models/Employee.go
@@ -1,6 +1,6 @@
 package models
 
 type Employee struct {
-	Id int `json:"id" gorm:"primaryKey"`
-	*User
+	Id       int  `json:"id" gorm:"primaryKey;autoIncrement"`
+	IdUserFk User `json:"id_user" gorm:"not null;unique;foreignKey:IdUserFk;references:Id"`
 }
