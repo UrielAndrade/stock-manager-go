f76afe9b4354ab318e3247ec5c6a866bbd609424
UrielAndrade <andradeleiru@gmail.com>
2026-05-20 11:41:33 -0400

    feat: update Product model to include auto-increment ID and foreign key for manufacturer

--- internal/models/Product.go
+++ internal/models/Product.go
@@ -1,10 +1,11 @@
 package models
 
 type Product struct {
-	ID             int     `json:"id" gorm:"primaryKey"`
-	Name           string  `json:"name"`
-	ManufacturerID int     `json:"manufacturer_id"`
-	Price          float64 `json:"price"`
-	Brand          string  `json:"brand"`
-	Quantity       int     `json:"quantity"`
+	ID               int          `json:"id" gorm:"primaryKey;autoIncrement"`
+	IdManufacturerFk int          `json:"manufacturer_id" gorm:"not null;column:id_manufacturer_fk"`
+	Manufacturer     Manufacturer `json:"manufacturer" gorm:"foreignKey:IdManufacturerFk;references:ID"`
+	Name             string       `json:"name"`
+	Price            float64      `json:"price"`
+	Brand            string       `json:"brand"`
+	Quantity         int          `json:"quantity"`
 }
