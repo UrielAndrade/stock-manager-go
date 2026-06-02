a13e61b55d4070c8a846d89f53c3ab6a2b28cc0a
UrielAndrade <andradeleiru@gmail.com>
2026-06-02 14:55:31 -0400

    refactor: remove unused Manufacturer model

--- internal/models/Manufacturer.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Manufacturer struct {
-	ID             int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name           string `json:"name" gorm:"index"`
-	Country        string `json:"country" gorm:"index"`
-	Email          string `json:"email" gorm:"unique"`
-	FoundationYear int    `json:"foundation_year" gorm:"index"`
-}
