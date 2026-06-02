bcad35350acad9b8580d2c02883685ff4d011b03
UrielAndrade <andradeleiru@gmail.com>
2026-06-02 14:55:14 -0400

    refactor: update product handler to use DTOs for Create, Get, and Update functions

--- internal/handlers/product_handler.go
+++ internal/handlers/product_handler.go
@@ -4,58 +4,83 @@ import (
 	"strconv"
 
 	"estoque-go/internal/database"
+	"estoque-go/internal/dtos"
 	"estoque-go/internal/models"
 
 	"github.com/go-fuego/fuego"
 )
 
-func CreateProduct(c fuego.ContextWithBody[models.Product]) (models.Product, error) {
-	product, err := c.Body()
+func toProductResponse(p models.Product) dtos.ProductResponseDTO {
+	return dtos.ProductResponseDTO{
+		ID:               p.ID,
+		IdManufacturerFk: p.IdManufacturerFk,
+		Name:             p.Name,
+		Price:            p.Price,
+		Brand:            p.Brand,
+		Quantity:         p.Quantity,
+	}
+}
+
+func CreateProduct(c fuego.ContextWithBody[dtos.CreateProductDTO]) (dtos.ProductResponseDTO, error) {
+	input, err := c.Body()
 	if err != nil {
-		return models.Product{}, err
+		return dtos.ProductResponseDTO{}, fuego.BadRequestError{Err: err}
 	}
 
-	if erro := database.DB.Create(&product).Error; erro != nil {
-		return models.Product{}, erro
+	product := models.Product{
+		IdManufacturerFk: input.IdManufacturerFk,
+		Name:             input.Name,
+		Price:            input.Price,
+		Brand:            input.Brand,
+		Quantity:         input.Quantity,
 	}
 
-	return product, nil
+	if err := database.DB.Create(&product).Error; err != nil {
+		return dtos.ProductResponseDTO{}, err
+	}
+
+	return toProductResponse(product), nil
 }
 
-func GetProducts(c fuego.ContextNoBody) ([]models.Product, error) {
+func GetProducts(c fuego.ContextNoBody) ([]dtos.ProductResponseDTO, error) {
 	var products []models.Product
-	if erro := database.DB.Find(&products).Error; erro != nil {
-		return nil, erro
+	if err := database.DB.Find(&products).Error; err != nil {
+		return nil, err
 	}
-	return products, nil
+
+	var response []dtos.ProductResponseDTO
+	for _, p := range products {
+		response = append(response, toProductResponse(p))
+	}
+	return response, nil
 }
 
-func UpdateProduct(c fuego.ContextWithBody[models.Product]) (models.Product, error) {
+func UpdateProduct(c fuego.ContextWithBody[dtos.UpdateProductDTO]) (dtos.ProductResponseDTO, error) {
 	id, _ := strconv.Atoi(c.PathParam("id"))
 
 	var product models.Product
 	if err := database.DB.First(&product, id).Error; err != nil {
-		return models.Product{}, fuego.NotFoundError{Err: err}
+		return dtos.ProductResponseDTO{}, fuego.NotFoundError{Err: err}
 	}
 
 	input, err := c.Body()
 	if err != nil {
-		return models.Product{}, fuego.BadRequestError{Err: err}
+		return dtos.ProductResponseDTO{}, fuego.BadRequestError{Err: err}
 	}
 
 	if err := database.DB.Model(&product).Updates(input).Error; err != nil {
-		return models.Product{}, err
+		return dtos.ProductResponseDTO{}, err
 	}
 
-	return product, nil
+	return toProductResponse(product), nil
 }
 
-func DeleteProduct(c fuego.ContextNoBody) (string, error) {
+func DeleteProduct(c fuego.ContextNoBody) (any, error) {
 	id, _ := strconv.Atoi(c.PathParam("id"))
 
 	if err := database.DB.Delete(&models.Product{}, id).Error; err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return "Deletado com sucesso", nil
+	return map[string]string{"message": "Deletado com sucesso"}, nil
 }
